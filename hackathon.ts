e6c132bf47553f78a61ee698cc6957de84a95ba9
Gidaio <killshade@gmail.com>
2019-09-20 17:58:54 -0600

    djikstra, in theory

--- hackathon.ts
+++ hackathon.ts
@@ -1,5 +1,7 @@
 import { readFileSync } from "fs"
 
+type Dimension = "x" | "y" | "z" | "v" | "w"
+
 interface Input {
   dimensions: string[]
   size: number
@@ -18,7 +20,7 @@ interface InputSpace {
   moves: string
 }
 
-interface MySpaces {
+interface SpaceWithPoints {
   x: number
   y: number
   z: number
@@ -27,6 +29,8 @@ interface MySpaces {
   moves: string
   pointsRemaining: number
 }
+
+type Space = InputSpace | SpaceWithPoints
 
 
 const input: Input = JSON.parse(readFileSync("input.json", "utf8"))
@@ -37,9 +41,51 @@ solveMaze(input)
 function solveMaze(input: Input) {
   const startingPoints = input.spaces.length
 
-  const visitedSpaces: MySpaces[] = [
-    { ...getSpaceByCoords(input.spaces, parseStringCoords(input.start)), pointsRemaining: startingPoints }
+  const spacesToVisit: SpaceWithPoints[] = [
+    { ...input.spaces[getIndexByCoords(parseStringCoords(input.start))], pointsRemaining: startingPoints }
   ]
+  const visitedSpaces: SpaceWithPoints[] = []
+
+  while (spacesToVisit.length) {
+    spacesToVisit.sort((a, b) => b.pointsRemaining - a.pointsRemaining)
+    const spaceToVisit = spacesToVisit.shift()!
+
+    const directions = spaceToVisit.moves.split("") as Dimension[]
+
+    for (const direction of directions) {
+      let potentialSpace = { ...spaceToVisit, pointsRemaining: spaceToVisit.pointsRemaining - 1 }
+      if (input.dimensions.includes(direction)) {
+        potentialSpace[direction]--
+      } else {
+        potentialSpace[direction.toLowerCase() as Dimension]++
+      }
+
+      // If we've already visited the space
+      const previouslyVisitedSpaceIndex = visitedSpaces.findIndex(isSameSpace(potentialSpace))
+      if (previouslyVisitedSpaceIndex !== -1) {
+        // If we got to it faster, remove it from the visited list and add it to the to visit list.
+        if (visitedSpaces[previouslyVisitedSpaceIndex].pointsRemaining < potentialSpace.pointsRemaining) {
+          visitedSpaces.splice(previouslyVisitedSpaceIndex, 1)
+          spacesToVisit.push(potentialSpace)
+        }
+        // Otherwise, ignore this.
+      } else {
+        // If we haven't visited the space.
+        const toVisitSpaceIndex = spacesToVisit.findIndex(isSameSpace(potentialSpace))
+        if (toVisitSpaceIndex !== -1) {
+          // If it's in the to visit list, keep the higher score.
+          spacesToVisit[toVisitSpaceIndex].pointsRemaining = Math.max(spacesToVisit[toVisitSpaceIndex].pointsRemaining, potentialSpace.pointsRemaining)
+        } else {
+          spacesToVisit.push(potentialSpace)
+        }
+      }
+    }
+  }
+}
+
+
+function buildCoords(space: Space): number[] {
+  return [space.x, space.y, space.z, space.v, space.w]
 }
 
 
@@ -52,8 +98,16 @@ function parseStringCoords(coords: string): number[] {
 }
 
 
-function getSpaceByCoords(spaces: InputSpace[], coords: number[]): InputSpace {
-  const index = coords[0] * 625 + coords[1] * 125 + coords[2] * 25 + coords[3] * 5 + coords[4]
+function isSameSpace(space: Space) {
+  return (candidateSpace: Space) =>
+    candidateSpace.x === space.x &&
+    candidateSpace.y === space.y &&
+    candidateSpace.z === space.z &&
+    candidateSpace.v === space.v &&
+    candidateSpace.w === space.w
+}
 
-  return spaces[index]
+
+function getIndexByCoords(coords: number[]): number {
+  return coords[0] * 625 + coords[1] * 125 + coords[2] * 25 + coords[3] * 5 + coords[4]
 }
